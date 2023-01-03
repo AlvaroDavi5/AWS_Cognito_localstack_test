b55d889d7922da85e46f4bf6f89891dea27985aa
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2023-01-03 13:55:34 -0300

    updated s3 script

--- S3_sample.sh
+++ S3_sample.sh
@@ -20,6 +20,15 @@ awslocal sqs get-queue-attributes --queue-url=http://localhost:4566/queue/test_q
 # Upload File to S3 Bucket
 awslocal s3 cp image.png s3://testbucket/ --endpoint-url=http://127.0.0.1:4566 --output=table | cat
 
+# Generate Link to File
+awslocal s3 presign s3://testbucket/image.png --expires-in=300 # in seconds
+
+# Download File by Link
+wget "http://localhost:4566/testbucket/image.png?AWSAccessKeyId=mock&Signature=xxx&Expires=1672756866"
+
 # List Received Messages from SQS
 awslocal sqs receive-message --queue-url=http://localhost:4566/queue/test_queue --attribute-names=All --message-attribute-names=All --endpoint-url=http://127.0.0.1:4566 --output=json | cat
+
+# Remove File from Bucket
+awslocal s3 rm s3://testbucket/image.png
 
